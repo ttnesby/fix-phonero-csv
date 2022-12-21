0c526d89cf7c3f047aec85bbbc3b9b49ab9e4c8d
Torstein Nesby <ttnesby@gmail.com>
2022-12-21 05:07:02 +0100

    mapping completed

--- src/Upload.elm
+++ src/Upload.elm
@@ -8,7 +8,52 @@ import File.Select as Select
 import Html exposing (Html, button, p, text)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
+import String
 import Task
+
+
+mapValue : Int -> String -> String
+mapValue colNo value =
+    if colNo == 0 || colNo == 1 then
+        "0"
+
+    else if colNo == 4 || colNo == 5 || colNo == 6 then
+        "0.00"
+
+    else if colNo == 16 then
+        "\"\""
+
+    else
+        value
+
+
+mapLine : String -> String
+mapLine str =
+    let
+        split =
+            String.split ";" str
+    in
+    if List.length split == 1 then --
+        ""
+
+    else if List.length split /= 17 then
+        String.concat [ String.fromInt <| List.length split, " columns, expected 17" ]
+
+    else
+        split
+            |> List.indexedMap mapValue
+            |> List.foldr (\x y -> x ++ ";" ++ y) ""
+            |> (\x -> String.dropRight 1 x) -- remove last semicolon
+
+
+mapCSV : List String -> String
+mapCSV csv =
+    case csv of
+        [] ->
+            ""
+
+        h :: t ->
+            (h |> mapLine) ++ "\n" ++ mapCSV t
 
 
 
@@ -30,7 +75,7 @@ main =
 
 
 type alias Model =
-    { csv : Maybe String
+    { csv : Maybe (List String)
     }
 
 
@@ -63,7 +108,7 @@ update msg model =
             )
 
         CsvLoaded content ->
-            ( { model | csv = Just content }
+            ( { model | csv = Just <| String.lines <| content }
             , Cmd.none
             )
 
@@ -79,7 +124,7 @@ view model =
             button [ onClick CsvRequested ] [ text "Load CSV" ]
 
         Just content ->
-            p [ style "white-space" "pre" ] [ text content ]
+            p [ style "white-space" "pre" ] [ text <| mapCSV <| content ]
 
 
 
